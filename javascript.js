43a377dcc0708134bff7b1704f6207597ab5fd89
tosatori <tosatori@proton.me>
2022-07-28 10:58:51 +0200

    Add function for computer player choice

---
+++ javascript.js
@@ -0,0 +1,14 @@
+/* Computer players choice gets randomly picked */
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3) + 1;
+    switch (num) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissor";
+    }
+}
+
