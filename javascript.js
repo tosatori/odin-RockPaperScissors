aaba73523562f1e49ae5dca0ddd48721789818b4
tosatori <tosatori@proton.me>
2022-10-10 16:32:16 +0200

    Remove getPlayerChoice() function

--- javascript.js
+++ javascript.js
@@ -12,19 +12,6 @@ function getComputerChoice() {
     }
 }
 
-/** Let player type in its choice. Make sure input is valid, if not repeat process. 
- * Return players choice in all lower case letters */
-
-function getPlayerChoice() {
-    while(true) {
-        let input = prompt("Please Enter your choice: ").toLowerCase();
-            if (input === "rock" || input === "paper" || input === "scissor") {
-                return input;
-            } else {
-                window.alert("Something went wrong. Please make sure to only choose between Rock, Paper and Scissor.");
-                }
-        }
-    }    
 
 /**
  * Check if player and computer have made the same choice. If so return true.
